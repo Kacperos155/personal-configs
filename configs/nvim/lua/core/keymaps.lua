3373f3a689aa4cf9202f250da128d601391e033b
Kacper Zielinski <KacperZ155@gmail.com>
2026-05-10 07:16:27 +0200

    [NeoVim] Map <Esc> to exit terminal mode

--- configs/nvim/lua/core/keymaps.lua
+++ configs/nvim/lua/core/keymaps.lua
@@ -6,3 +6,6 @@ vim.g.maplocalleader = ' '
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Use <Esc> to exit terminal mode
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
