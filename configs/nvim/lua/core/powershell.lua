34ff61ee9d223ee37477cb6007a428cca4225287
Kacper Zielinski <KacperZ155@gmail.com>
2026-05-10 05:53:23 +0200

    [NeoVim] Improve PowerShell config
    by introducing changes from https://neovim.io/doc/user/options/#shell-pwsh

--- configs/nvim/lua/core/powershell.lua
+++ configs/nvim/lua/core/powershell.lua
@@ -1,7 +1,9 @@
--- Use PowerShell as the default shell
+-- Use PowerShell 7+ as the default shell.
 vim.o.shell = "pwsh"
-vim.o.shellcmdflag = "-NoLogo -NoProfile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.o.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-vim.o.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.o.shellcmdflag = [[
+-NoLogo -NoProfile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new(); $PSDefaultParameterValues['Out-File:Encoding']='utf8'; $PSStyle.OutputRendering='PlainText';
+]]
+vim.o.shellpipe = "> %s 2>&1"
+vim.o.shelltemp = false
 vim.o.shellquote = ""
 vim.o.shellxquote = ""
