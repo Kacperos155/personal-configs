baf16b8583c0ac0353d88b987c93488f9780c3b0
Kacper Zielinski <KacperZ155@gmail.com>
2026-06-02 03:03:37 +0200

    [NVim] Map `<leader>ta` to toggle aerial window

--- configs/nvim/lua/plugins/aerial.lua
+++ configs/nvim/lua/plugins/aerial.lua
@@ -3,4 +3,13 @@
 return {
   'stevearc/aerial.nvim',
   opts = {},
+
+  config = function(_, opts)
+    local Aerial = require('aerial')
+    Aerial.setup(opts)
+
+    vim.keymap.set('n', '<leader>ta', function()
+      Aerial.toggle({focus = false})
+    end, {desc = 'Toggle [a]erial / symbols window'})
+  end,
 }
