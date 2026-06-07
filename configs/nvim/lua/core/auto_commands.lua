fbf6716c863c50a55617b8cbb1253d05d40fd175
Kacper Zielinski <KacperZ155@gmail.com>
2026-06-07 17:06:31 +0200

    [NVim] Auto-open quickfix and loclist after grep

--- configs/nvim/lua/core/auto_commands.lua
+++ configs/nvim/lua/core/auto_commands.lua
@@ -6,3 +6,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+-- Auto-open quickfix/loclist window when grep returns at least one result.
+vim.api.nvim_create_autocmd('QuickFixCmdPost', {
+  desc = 'Open quickfix window after grep with non-zero results',
+  pattern = { 'grep', 'grepadd', 'vimgrep', 'vimgrepadd' },
+  command = 'cwindow',
+})
+vim.api.nvim_create_autocmd('QuickFixCmdPost', {
+  desc = 'Open loclist window after lgrep with non-zero results',
+  pattern = { 'lgrep', 'lgrepadd', 'lvimgrep', 'lvimgrepadd' },
+  command = 'lwindow',
+})
