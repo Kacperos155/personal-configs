fa0950f03a4540a995d0126fc8df62b41c5a5328
Kacper Zielinski <KacperZ155@gmail.com>
2026-06-26 14:51:07 +0200

    [NVim] Set cursor line number color to green in Catppuccin themes

--- configs/nvim/lua/plugins/theme.lua
+++ configs/nvim/lua/plugins/theme.lua
@@ -8,10 +8,18 @@ return {
   -- It's recommended to set this to a high number for colorschemes.
   priority = 1000,
 
+  opts = {
+    highlight_overrides = {
+      all = function(colors)
+        return {
+          CursorLineNr = { fg = colors.green },
+        }
+      end,
+    },
+  },
+
   config = function(_, opts)
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin-mocha")
   end,
-
-  opts = {},
 }
