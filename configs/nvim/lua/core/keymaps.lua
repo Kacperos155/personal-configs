baf981162d6c8b74af405b71dfbb365790e4c5e8
Kacper Zielinski <KacperZ155@gmail.com>
2026-07-19 13:03:37 +0200

    [NVim] Map <leader>oe to open empty buffer

--- configs/nvim/lua/core/keymaps.lua
+++ configs/nvim/lua/core/keymaps.lua
@@ -62,6 +62,7 @@ vim.keymap.set("n", "<leader>td", function()
 end, { desc = "Toggle [d]iff mode" })
 
 -- Open <something>.
+vim.keymap.set("n", "<leader>oe", "<cmd>enew<CR>", { desc = "Open [e]mpty buffer" })
 vim.keymap.set("n", "<leader>oy", function()
   local yazi = require("yazi")
   yazi.pick_files()
