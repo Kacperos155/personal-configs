533a34644df41b551aa7912de1cfe5ef3ba8cdbd
Kacper Zielinski <KacperZ155@gmail.com>
2026-08-04 10:22:12 +0200

    [NVim] Extract `folke/lazy.nvim` setup from main `init.lua`

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,52 +1,12 @@
+-- Core settings.
 require("core.powershell")
 require("core.options")
 require("core.keymaps")
 require("core.auto_commands")
 require("core.user_commands")
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
-end
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
-
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
-require("lazy").setup({
-  require("plugins.theme"),
-  require("plugins.mason"),
-  require("plugins.lualine"),
-  require("plugins.treesitter"),
-  require("plugins.git"),
-  require("plugins.aerial"),
-  require("plugins.conform"),
-  require("plugins.csvview"),
-  require("plugins.fff"),
-  require("plugins.indent-blankline"),
-  require("plugins.render-markdown"),
-  require("plugins.mini"),
-  require("plugins.misc"),
-  require("plugins.quicker"),
-  require("plugins.which-key"),
-})
+-- Setup plugin manager.
+require("config.lazy")
 
 -- Enable Treesitter syntax parsing.
 -- Parsers are asynchronously installed by the 'nvim-treesitter/nvim-treesitter' plugin.
