9e842b8488f4869a8c2ce87ef7143f25b8025940
Kacper Zielinski <KacperZ155@gmail.com>
2026-09-15 00:03:28 +0200

    [NVim] Add rumdl formatter for Markdown and Git commits

--- configs/nvim/lua/plugins/conform.lua
+++ configs/nvim/lua/plugins/conform.lua
@@ -13,12 +13,28 @@ return {
     -- The following formatters can be installed via mason.nvim.
     formatters_by_ft = {
       -- stylua: ignore start
-      cpp     = { "clang-format", timeout_ms = 3000 },
-      html    = { "djlint", timeout_ms = 3000 },
-      json    = { "jq" },
-      lua     = { "stylua" },
-      python  = { "ruff_format" },
+      cpp       = { "clang-format", timeout_ms = 3000 },
+      gitcommit = { "rumdl" },
+      html      = { "djlint", timeout_ms = 3000 },
+      json      = { "jq" },
+      lua       = { "stylua" },
+      markdown  = { "rumdl" },
+      python    = { "ruff_format" },
       -- stylua: ignore end
+    },
+
+    formatters = {
+      rumdl = {
+        inherit = "rumdl",
+        append_args = {
+          "--config",
+          "MD060.enabled = true", -- https://rumdl.dev/md060/ | Enable table formatting.
+          "--config",
+          "MD060.style = 'aligned'", -- https://rumdl.dev/md060/ | Table columns are always padded.
+          "--config",
+          "MD022.lines-below = 0", -- https://rumdl.dev/md022/ | Do not add blank lines below headings.
+        },
+      },
     },
   },
 
